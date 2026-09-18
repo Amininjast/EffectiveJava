84b8023819685ea34500c7fd7719f0b49e6c4902
Amininjast <aminarad228@gmail.com>
2026-09-18 19:15:57 +0330

    Singleton

---
+++ src/s1/singleton/Database.java
@@ -0,0 +1,13 @@
+package s1.singleton;
+
+public class Database {
+    private static Database instance;
+    private Database(){}
+
+    public static Database getInstance(){
+        if (instance == null){
+            instance = new Database();
+        }
+        return instance;
+    }
+}
